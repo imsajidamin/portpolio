83746f3e6f890f8c8c5195c3bc394b19b9f56d1c
imsajidamin <imsajidamin@gmail.com>
2025-02-22 12:10:14 +0500

    js

--- js/mail.js
+++ js/mail.js
@@ -1,10 +1,10 @@
 function sendMail(){
     let parms = {
         name : document.getElementById("name").value,
-        name : document.getElementById("email").value,
-        name : document.getElementById("phone").value,
-        name : document.getElementById("subject").value,
-        name : document.getElementById("message").value
+        email : document.getElementById("email").value,
+        phone : document.getElementById("phone").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value
 
     }
     emailjs.send("service_6m84kiu","template_pl7fopi",parms).then(alert("Email send!!!"));
