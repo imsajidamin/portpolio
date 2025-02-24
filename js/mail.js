6295e7124bc65b3778bc961aa9538e556cc63fb4
imsajidamin <imsajidamin@gmail.com>
2025-02-24 16:49:14 +0500

    keywords

--- js/mail.js
+++ js/mail.js
@@ -1,11 +1,19 @@
-function sendMail(){
-    let parms = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        phone : document.getElementById("phone").value,
-        subject : document.getElementById("subject").value,
-        message : document.getElementById("message").value
-
+function sendMail() {
+    alert('kkkkkk');
+    let para = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value
     }
-    emailjs.send("service_6m84kiu","template_pl7fopi",parms).then(alert("Email send!!!"));
+    if (para.name == ""||para.email == ""||para.phone == ""||para.subject == ""||para.message == "") {
+        
+        document.getElementById("formerror").innerText = "field Required !";
+        return false;
+    
+    } else {
+        emailjs.send("service_6m84kiu", "template_pl7fopi", para).then(alert("Email send!!!"));
+    }
+
 }
