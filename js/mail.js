710387cdcfbe94010982d84b0e7bc4a55c8e40bb
imsajidamin <imsajidamin@gmail.com>
2025-02-24 19:09:45 +0500

    seo

--- js/mail.js
+++ js/mail.js
@@ -1,19 +1,22 @@
 function sendMail() {
-    alert('kkkkkk');
+    
     let para = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         phone: document.getElementById("phone").value,
         subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value
+        message: document.getElementById("message").value,
     }
-    if (para.name == ""||para.email == ""||para.phone == ""||para.subject == ""||para.message == "") {
-        
-        document.getElementById("formerror").innerText = "field Required !";
-        return false;
     
-    } else {
-        emailjs.send("service_6m84kiu", "template_pl7fopi", para).then(alert("Email send!!!"));
-    }
+    // if (para.name == ""||para.email == ""||para.phone == ""||para.subject == ""||para.message == "") {
+        
+    //     document.getElementById("formerror").innerText = "field Required !";
+    //     alert('kkkkk');
+    //     return false;
+        
+    
+    // } else {
+        emailjs.send("service_6m84kiu","template_pl7fopi",para).then(alert("Email send!!!"));
+    
 
 }
